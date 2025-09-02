4d9cd042d24b407cd27b7e5a6c7746b2f11a1055
Artreeus <142108262+Artreeus@users.noreply.github.com>
2025-09-03 00:06:36 +0600

    Feat : Some changes made in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Bitsec Dashboard',
+  description: 'Bitsec Dashboard',
+  generator: 'Bitsec Dashboard',
 }
 
 export default function RootLayout({
